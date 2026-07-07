87e1d2fd423f689b448a965c4327d5c266e4f6ea
Taci <TU_EMAIL_ACA>
2026-07-07 11:20:13 -0300

    saco archivos ignorados

--- proyecto-final/edge-module/managed_components/jgromes__radiolib/extras/test/ci/version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-grep -zaoP 'RadioLib Info[\x20-\x7E\s]*' "$1"
-echo ""
--- proyecto-final/gateway-module/managed_components/jgromes__radiolib/extras/test/unit/tests/main.cpp
+++
@@ -1,5 +0,0 @@
-#define BOOST_TEST_MODULE "RadioLib Unit test"
-#include <boost/test/included/unit_test.hpp>
-
-// intentionally left blank, boost.test creates its own entrypoint
-
